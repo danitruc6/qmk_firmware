b891a1ed9c0e72806989d586f6ce2d36cc0e7432
danitruc6 <danitruc6@hotmail..com>
2021-05-08 16:45:16 -0600

    cleaning the code

--- keyboards/gmmk/pro/keymaps/danitruc6/keymap.c
+++ keyboards/gmmk/pro/keymaps/danitruc6/keymap.c
@@ -16,7 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Prt           Rotary(Mute)
